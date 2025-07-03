728b802f9be33d6b8a8469ee7221b52dece4c0b9
Alexandre <alevaristofig@gmail.com>
2025-07-03 14:53:51 -0300

    Criação do projeto

---
+++ src/main/java/com/algacomments/AlgacommentsApplication.java
@@ -0,0 +1,13 @@
+package com.algacomments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgacommentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgacommentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algacomments/AlgacommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algacomments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgacommentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
