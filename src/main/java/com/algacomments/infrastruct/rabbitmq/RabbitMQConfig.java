05e7114080e15deafed487a90219aa0420f9e625
Alexandre <alevaristofig@gmail.com>
2025-07-11 11:49:45 -0300

    Criação do exchange e da queue de comment, e do binding do exchange e do queue

--- src/main/java/com/algacomments/infrastruct/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/algacomments/infrastruct/rabbitmq/RabbitMQConfig.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -15,6 +19,7 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 public class RabbitMQConfig {
 
 	public static final String FANOUT_EXCHANGE_NAME = "comments-processing.comments-received.v1.e";
+	public static final String QUEUE_COMMENTS_PROCESSING = "comments-processing.comments-process.v1.q";
 	
 	@Bean
 	public Jackson2JsonMessageConverter jackson2JsonMessageConverter(ObjectMapper mapper) {
@@ -29,5 +34,15 @@ public class RabbitMQConfig {
 	@Bean
 	public FanoutExchange exchange() {
 		return ExchangeBuilder.fanoutExchange(FANOUT_EXCHANGE_NAME).build();
+	}
+	
+	@Bean
+	public Queue queueComment() {
+		return QueueBuilder.durable(QUEUE_COMMENTS_PROCESSING).build();
+	}
+	
+	@Bean
+	public Binding bindingCommentProcess() {
+		return BindingBuilder.bind(queueComment()).to(exchange());
 	}
 }
